a0a23cb9e983154b94b2fe0a4dbabd929905ea60
GomezMunera <gomezmunera@corum.org.co>
2023-05-15 22:33:32 -0500

    update path deployment

--- deployment.sh
+++ deployment.sh
@@ -2,5 +2,5 @@ git pull
 bundle exec jekyll build
 docker build . -t corumcorp/corumpage:latest
 docker push corumcorp/corumpage:latest
-microk8s kubectl delete -f deployment.yaml
-microk8s kubectl apply -f deployment.yaml
+microk8s kubectl delete -f deployment/deployment.yaml
+microk8s kubectl apply -f deployment/deployment.yaml
